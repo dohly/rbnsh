6d6a2d7b769c67158d7007993ace6148607b0a68
Ivan <1271285+dohly@users.noreply.github.com>
2020-12-17 22:59:43 +0500

    kartinka

--- projects/code.js
+++ projects/code.js
@@ -46,7 +46,19 @@ handlers[GREEN] = function () {
   golova.Pryamo();
 };
 handlers[PLAY]=function(){
-  
+  screen.setFontVector(15);
+  screen.clear();
+  if (y_strelki==20){
+    var img = {
+  width : 128, height : 64, bpp : 1,
+  buffer : require("heatshrink").decompress(atob("AA0f/ANLgP/AAWAB5N/B4f+BxEPBwYAB+APHBYXgg4ECBwwKCCovgB4s/BAoWB/guGA4wXBMQgXBE4wIGj5HHFAKUEv6aIj6CEFw4wDIAUDVBK2B4CcCU5I6BUQTZLBYc/HxBACFYQzDgIXCj5LEIYQHDUIU/C4YMBCQQnFv43Dv/ADQZjBCgT+EJgMHLwYPIj/gh4PFIgIPEh/wj4GEB5H4EIIvLHoKOEB5BdBOwZvJB4JxBAwYPCDAkB/1/doavHB44AIB6H/B57eDABQPQH75veB4KmDB5S2DVoQADj7fEawIUBB4o6CB4MfB4MDfoR7EFQMH/Ef+BkCKYgGDh/4AIIYCEYIACEwcP+AhBHAQjBAAUPIwQ9BIIIFBn4DCAopdBEohADHwIlCRoJFDCYI0Cj5eCBgYGCeoXwEYIoCFgaQCAYIMBAAI+Cg5SDFYUHB4YXCBYZmDGAQuDPAgzBLgQEBDwZOBdgadFAAY2BXAd/dww+CdAh4BB46GDEoYwGBA55DYAj3FA4QXEDwIXGRoQmENBCcDUQZXHZYQADTgQAGv4ODPggAFVAIACLgqaHUg4A=="))
+};
+   
+    screen.drawImage(img, 0,0);
+  } else{
+    screen.drawString("=(", 0, 0);
+  }
+  screen.flip();
 };
 
 var pult = require("Pult").connect(P3, handlers, true);
