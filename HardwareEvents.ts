0f897085c987d80f1e6821a3315c97cb344180a6
Ivan <1271285+dohly@users.noreply.github.com>
2021-02-06 22:40:16 +0500

    debounce ircodes

--- HardwareEvents.ts
+++ HardwareEvents.ts
@@ -2,7 +2,7 @@ import { EventStream } from "./SimpleEventStream";
 
 export const HardwareEvents = {
     oledReady: new EventStream(),
-    irCodes: new EventStream<any>(),
+    irCodes: new EventStream<any>(0.1),
     leftWheelDone: new EventStream<number>(),
     rightWheelDone: new EventStream<number>(),
   };
--- SimpleEventStream.ts
+++ SimpleEventStream.ts
@@ -1,8 +1,21 @@
 type Subscription<TEvent> = (event?: TEvent) => void;
 export class EventStream<TEvent> {
+  private previousTime: number;
+  constructor(private debounceTime?: number) {}
   private subscribers: Array<Subscription<TEvent>> = [];
   public subscribe = (s: Subscription<TEvent>) => this.subscribers.push(s);
   public unsubscribe = (s: Subscription<TEvent>) =>
     (this.subscribers = this.subscribers.filter((x) => x !== s));
-  public publish = (e?: TEvent) => this.subscribers.forEach((x) => x(e));
+  public publish = (e?: TEvent) => {
+    if (this.debounceTime) {
+      const time = getTime();
+      const difference = this.previousTime ? time - this.previousTime : this.debounceTime;
+      if (difference >= this.debounceTime) {
+        this.previousTime = time;
+        this.subscribers.forEach((x) => x(e));
+      }
+    } else {
+      this.subscribers.forEach((x) => x(e));
+    }
+  };
 }
